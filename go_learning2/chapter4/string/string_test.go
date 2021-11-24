4717d4b499afd37ca54f8f3e74e2e60f94e7b9ab
sinkhaha <1468709106@qq.com>
2021-11-24 22:15:54 +0800

    fix

--- go_learning2/chapter4/string/string_test.go
+++ go_learning2/chapter4/string/string_test.go
@@ -1,26 +1,26 @@
 package string_test
 
 import (
-	"strings"
 	"strconv"
-	"testing" 
+	"strings"
+	"testing"
 )
 
 // 测试string的初始化
 func TestStringInit(t *testing.T) {
-   var s string
-   t.Log(s) // 初始化为默认值""
+	var s string
+	t.Log(s) // 初始化为默认值""
 
-   s = "hello"
-   t.Log(s)
-   t.Log(len(s)) // 5 指3个byte数，并不是字符
+	s = "hello"
+	t.Log(s)      // hello
+	t.Log(len(s)) // 5 指5个byte数，并不是字符
 
-   s = "中"
-   t.Log(len(s)) // 3 指3个byte数，并不是字符数
+	s = "中"
+	t.Log(len(s)) // 3 指3个byte数，并不是字符数
 
-   s = "\xE4\xB8\xA5" // 十六进制串
-   t.Log(s) // 严
-   t.Log(s[1])
+	s = "\xE4\xB8\xA5" // 十六进制串
+	t.Log(s)           // 严
+	t.Log(s[1])        // 184
 }
 
 // 测试strings包的函数
@@ -28,13 +28,13 @@ func TestStringFn(t *testing.T) {
 	str := "a,b,c"
 
 	parts := strings.Split(str, ",")
-	for _,v := range parts {
+	for _, v := range parts {
 		t.Log(v)
 	}
 
 	t.Log(strings.Join(parts, "-")) // a-b-c
 
-    // 数字10转字符串
+	// 数字10转字符串
 	s := strconv.Itoa(10)
 	t.Log("str" + s)
 
