aa726cb58c52dfd2743ed62964a27f5e25069d12
sinkhaha <1468709106@qq.com>
2021-11-24 22:33:42 +0800

    typestruct

--- go_learning2/chapter6/encapsulation/encap_test.go
+++ go_learning2/chapter6/encapsulation/encap_test.go
@@ -1,38 +1,43 @@
 package encapsulation_test
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 // 结构体定义
 type Employee struct {
-	Id string // 不需要逗号
+	Id   string // 不需要逗号
 	Name string
-	Age int
+	Age  int
 }
 
 // 测试实例化结构体
-func TestCreateEmplyeeObj(t *testing.T) {
+func TestCreateEmployeeObj(t *testing.T) {
 	// 实例方式一
 	e1 := Employee{"0", "张三", 20}
 	t.Log(e1) // {0 张三 20}
+	e11 := &e1
+	e11.Id = "11" // 改变了e1地址上Id的值，所以会影响到e1的Id的值
+	t.Log(e1)     // {11 张三 20}
+	t.Log(e11)    // &{11 张三 20}
 
 	// 实例方式二
 	e2 := Employee{Name: "李四", Age: 30}
 	t.Log(e2) // { 李四 30}
 
-	// 实例方式三
-	e3 := new(Employee) // 这里返回引用，相当于e := &Employee{}
-	t.Log(e3) // &{  0}
+	// 实例方式三 获取地址
+	e3 := new(Employee) // 这里返回引用，等价于e3 := &Employee{}
+	t.Log(e3)           // &{  0}
+
 	e3.Id = "4"
 	e3.Name = "老五"
 	e3.Age = 40
 	t.Log(e3) // &{4 老五 40}
 
-	t.Logf("e的类型是 %T", e1) // encapsulation_test.Employee
+	t.Logf("e的类型是 %T", e1)      // encapsulation_test.Employee
 	t.Logf("e3的类型是指针类型 %T", e3) // *encapsulation_test.Employee
-	t.Logf("e的类型是 %T", &e1) // 加个取指符  *encapsulation_test.Employee
+	t.Logf("e的类型是 %T", &e1)     // 加个取指符  *encapsulation_test.Employee
 }
 
 // 行为(方法)的定义一，此方式会复制e对象，此时e是一个新的地址
@@ -47,7 +52,7 @@ func (e *Employee) MyString2() string {
 	return fmt.Sprintf("ID是%s,名字是%s,年龄是%d", e.Id, e.Name, e.Age)
 }
 
-func TestEmplyeeFunc(t *testing.T) {
+func TestEmployeeFunc(t *testing.T) {
 	e1 := Employee{"0", "张三", 20}
 	// 通过实例访问
 	t.Log(e1.MyString()) // ID是0,名字是张三,年龄是20
